58a0f20210593e64c785caef706333794f336d85
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-01-21 20:00:13 +0300

    Second task

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -3,3 +3,21 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите  коофецент k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  коофецент b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  коофецент k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  коофецент b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+else if (k1 == k2) Console.WriteLine("Прямые параллельны");
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"{x} & {y}");
+}
